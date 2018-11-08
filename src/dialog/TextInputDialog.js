5ef03db05c64ed085b6ec65f3ef2249ba9fbed66
Huy Quang <>
2018-11-08 09:37:17 +0700

    Fix

--- src/dialog/TextInputDialog.js
+++ src/dialog/TextInputDialog.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Modal from "react-native-modal";
 
-
 class TextInputDialog extends Component {
   constructor(props) {
     super(props);
