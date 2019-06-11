a9b29a96c20b7418b41e8df7cf12daabe118d966
Huy Quang <>
2019-06-11 10:45:55 +0700

    Open numpad

--- src/dialog/index.js
+++ src/dialog/index.js
@@ -3,11 +3,13 @@ import NumbericInputDialog from "./NumbericInputDialog";
 import SelectionInputDialog from "./SelectionInputDialog";
 import MultiSelectionInputDialog from "./MultiSelectionInputDialog";
 import DateInputDialog from "./DateInputDialog";
+import NumPad from "./NumbericInputDialog/Numpad";
 
 export {
   TextInputDialog,
   NumbericInputDialog,
   SelectionInputDialog,
   MultiSelectionInputDialog,
-  DateInputDialog
+  DateInputDialog,
+  NumPad
 };
